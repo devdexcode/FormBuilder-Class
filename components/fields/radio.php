67cc8571034dd3757a1fe366d42577d659e47ee8
= <=>
2022-08-02 06:31:41 +0500

    added multiselect

--- components/fields/radio.php
+++ components/fields/radio.php
@@ -2,7 +2,7 @@
   <label class="<?=$input_class != "" ? $input_class:''; ?> "> <?=ucfirst($option);?> <input type="radio" 
   class="<?=$the_id?> input-<?=$type;?> field_<?=$the_id?>" id="<?=$the_id?>" 
   name="<?=$the_name?>" 
-  <?php echo (@$dbval == "yes"?'checked="checked"':(isset($_REQUEST[$the_name]) ? 'checked="checked"':''));?>
+  <?php echo (@$dbval == $option?'checked="checked"':($_REQUEST[$the_name]==$option? 'checked="checked"':''));?>
   data-id="<?=$the_name?>"
   value="<?=$option;?>"></label>
 <?php endforeach;?>
